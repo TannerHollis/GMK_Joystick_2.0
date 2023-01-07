f6851f2ded70a87f52ebd317684430a50bb5873f
TannerHollis <40923236+TannerHollis@users.noreply.github.com>
2023-01-07 17:52:07 -0500

    ADC 16 cyc to 9 cyc

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -284,7 +284,7 @@ static void MX_ADC_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_16CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_9CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
   {
     Error_Handler();
